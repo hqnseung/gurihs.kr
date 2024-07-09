ad6f0fa7954a67824f81b2d73715cc05f52081b7
hqnseung <sw@gurihs.kr>
2024-07-09 11:20:13 +0900

    test: 10시 cron 스케쥴 변경

--- src/cron.js
+++ src/cron.js
@@ -1,24 +1,22 @@
 const { default: axios } = require('axios');
 const cron = require('node-cron');
 
-function exampleFunction() {
-    axios.get(url)
-        .then(async response => {
-            if (response.data.RESULT && response.data.RESULT.CODE === "INFO-200") {
-                return
-            } else {
-                const data = response.data;
-                const ddishNm = data.mealServiceDietInfo[1].row[0].DDISH_NM;
-                console.log(ddishNm)
-            }
-        }).catch(error => {
-            console.error("Error fetching the lunch menu:", error);
-        });
-}
-
 cron.schedule('0 10 * * *', () => {
     console.log('10시 작업이 시작되었습니다.');
-    exampleFunction();
+    axios.get(url)
+    .then(async response => {
+      let lunch = "";
+      if (response.data.RESULT && response.data.RESULT.CODE === "INFO-200") {
+        lunch = "오늘의 급식정보가 존재하지 않습니다.";
+      } else {
+        const data = response.data;
+        const ddishNm = data.mealServiceDietInfo[1].row[0].DDISH_NM;
+        lunch = ddishNm;
+      }
+      console.log(lunch)
+    }).catch(error => {
+      console.error("Error fetching the lunch menu:", error);
+    });
 }, {
     scheduled: true,
     timezone: "Asia/Seoul" 
